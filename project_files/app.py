4a4257e574248769044ce03d133af144d47fa492
Aaron Wierengo <awierengo@utexas.edu>
2019-04-19 17:55:34 -0500

    added all time averages and pick-a-stat, (ML WIP)

--- project_files/app.py
+++ project_files/app.py
@@ -41,7 +41,7 @@ def home():
 
 @app.route("/playoff_average")
 def playoffs():
-    #Averages for playoff teams
+    #Averages for playoff teams by year
     results = db.session.query(Baseball_Data.Year,func.avg(Baseball_Data.RS),func.avg(Baseball_Data.RA),\
     func.avg(Baseball_Data.W),func.avg(Baseball_Data.OBP),func.avg(Baseball_Data.SLG),func.avg(Baseball_Data.BA)).\
     group_by(Baseball_Data.Year).filter(Baseball_Data.Playoffs == 1).all()
@@ -60,10 +60,31 @@ def playoffs():
 
     return jsonify(baseball_data)
 
+@app.route("/alltime_playoff")
+def alltime():
+    #All time Averages for a playoff making team
+    results = db.session.query(func.avg(Baseball_Data.RS),func.avg(Baseball_Data.RA),\
+    func.avg(Baseball_Data.W),func.avg(Baseball_Data.OBP),func.avg(Baseball_Data.SLG),func.avg(Baseball_Data.BA)).\
+    filter(Baseball_Data.Playoffs == 1).all()
+
+    baseball_data = []
+    for result in results:
+        baseball_dict = {}
+        baseball_dict["RS"] = result[0]
+        baseball_dict["RA"] = result[1]
+        baseball_dict["W"] = result[2]
+        baseball_dict["OBP"] = result[3]
+        baseball_dict["SLG"] = result[4]
+        baseball_dict["BA"] = result[5]
+        baseball_data.append(baseball_dict)
+
+    return jsonify(baseball_data)
+
 @app.route("/<team>")
 def team(team):
+    #Historical data on given team
     results = db.session.query(Baseball_Data.Year,Baseball_Data.W,Baseball_Data.Playoffs,\
-    Baseball_Data.RankPlayoffs,Baseball_Data.Team).filter(Baseball_Data.Team == team)
+    Baseball_Data.RankPlayoffs,Baseball_Data.Team).filter(Baseball_Data.Team == team).all()
 
     baseball_data = []
     for result in results:
@@ -79,8 +100,9 @@ def team(team):
 
 @app.route("/playoffs")
 def playoff_count():
+    #Historical playoff appearances by team
     results = db.session.query(Baseball_Data.Team,func.sum(Baseball_Data.Playoffs)).\
-    group_by(Baseball_Data.Team)
+    group_by(Baseball_Data.Team).all()
 
     baseball_data = []
     for result in results:
@@ -93,17 +115,32 @@ def playoff_count():
 
 @app.route("/stats/<stat>")
 def stats(stat):
-    test = f"Baseball_Data.{stat}"
-    results = db.session.query(Baseball_Data.Year,func.avg(test)).\
+
+    #Yearly average for given stat for playoff making team
+    results = db.session.query(Baseball_Data.Year,func.avg(Baseball_Data.RS),func.avg(Baseball_Data.RA),\
+    func.avg(Baseball_Data.W),func.avg(Baseball_Data.OBP),func.avg(Baseball_Data.SLG),func.avg(Baseball_Data.BA)).\
     group_by(Baseball_Data.Year).filter(Baseball_Data.Playoffs == 1)
 
     baseball_data = []
     for result in results:
         baseball_dict = {}
         baseball_dict["Year"] = result[0]
-        baseball_dict[f"Average_{stat}"] = result[1]
+        if stat == "RS":
+            baseball_dict[f"Average_{stat}"] = result[1]
+        elif stat == "RA":
+            baseball_dict[f"Average_{stat}"] = result[2]
+        elif stat == "W":
+            baseball_dict[f"Average_{stat}"] = result[3]
+        elif stat == "OBP":
+            baseball_dict[f"Average_{stat}"] = result[4]
+        elif stat == "SLG":
+            baseball_dict[f"Average_{stat}"] = result[5]
+        elif stat == "BA":
+            baseball_dict[f"Average_{stat}"] = result[6]
+        else:
+            baseball_dict["ERROR"] = f"No stat of type: {stat}"
+
         baseball_data.append(baseball_dict)
-    # return test
     return jsonify(baseball_data)
 
 
